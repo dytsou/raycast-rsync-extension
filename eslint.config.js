cb09147a2ea262c8807a592d259cc0d2ba6a2b3c
dytsou <d.y.tsou@gmail.com>
2026-02-03 18:31:10 +0800

    chore: Refactor ESLint configuration to streamline extends and improve readability in eslint.config.js.

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,19 @@
-const {
-    defineConfig,
-} = require("eslint/config");
+const { defineConfig } = require("eslint/config");
+const raycastConfig = require("@raycast/eslint-config");
 
-const js = require("@eslint/js");
-
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-module.exports = defineConfig([{
-    extends: ["@raycast"],
-}, {
+module.exports = defineConfig([
+  ...raycastConfig,
+  {
     files: ["**/*.test.ts", "**/__mocks__/**", "**/__tests__/**"],
-
     rules: {
-        "@typescript-eslint/no-explicit-any": "off",
-
-        "@typescript-eslint/no-unused-vars": ["error", {
-            argsIgnorePattern: "^_",
-            varsIgnorePattern: "^_",
-        }],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
     },
-}]);
+  },
+]);
